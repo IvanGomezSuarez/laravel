fc77c41945f004fa3456402c4852f19a914d27ea
IvanGomezSuarez <59507021+IvanGomezSuarez@users.noreply.github.com>
2020-11-05 20:42:25 +0100

    cambios en listado por error en cabeceras

--- p6/sistema/includes/scripts.php
+++ p6/sistema/includes/scripts.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" type = "text/css" href="css/styleAdmin.css">
 <link rel="stylesheet" type = "text/css" href="css/styleCurso.css">
 <link rel="stylesheet" type = "text/css" href="sistema/css/stylelogin.css">
-<link rel="stylesheet" type = "text/css" href="../sistema/css/style_list_usuario.css">
+<link rel="stylesheet" type = "text/css" href="css/style_list_usuario.css">
 <link rel="stylesheet" type = "text/css" href="../sistema/css/styleDeleteAdmin.css">
 <script src="js/jquery.min.js"></script>
 <script src="js/moment.min.js"></script>
--- p6/sistema/listado_usuarios.php
+++ p6/sistema/listado_usuarios.php
@@ -6,7 +6,7 @@ include "../conexion.php";
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <?php include "includes/scripts.php"; ?>
 	<title>Administracion</title>
 </head>
@@ -21,12 +21,13 @@ include "../conexion.php";
                 <th>Nombre</th>
                 <th>Apellido</th>
                 <th>DNI</th>
-                <th>Email</th>
                 <th>Username</th>
+                <th>Email</th>
                 <th>Acciones</th>
             </tr>
             <?php
-            $query = mysqli_query($conection, "SELECT id_user_admin,username, name, email, surname,dni FROM users_admin WHERE status = 1 ORDER BY id_user_admin ASC");
+            
+            $query = mysqli_query($conection, "SELECT id_user_admin,name,surname,dni,username,email FROM users_admin WHERE status = 1 ORDER BY id_user_admin ASC");
             $result = mysqli_num_rows($query);
             if($result > 0){
                 while ($data = mysqli_fetch_array($query)){
@@ -35,8 +36,8 @@ include "../conexion.php";
                     <td><?php echo $data["id_user_admin"]; ?></td>
                     <td><?php echo $data["name"]; ?></td>
                     <td><?php echo $data["surname"]; ?></td>
-                    <td><?php echo $data["username"]; ?></td>
                     <td><?php echo $data["dni"]; ?></td>
+                    <td><?php echo $data["username"]; ?></td>
                     <td><?php echo $data["email"] ?></td>
                     <td>
                         <a class="link_edit" href="edit_admin.php?id=<?php echo $data["id_user_admin"];?>">Editar</a>
@@ -53,6 +54,7 @@ include "../conexion.php";
             }
             ?>
         </table>
+        </div>
 	</section>
 	<?php include "includes/footer.php"; ?>
 </body>
