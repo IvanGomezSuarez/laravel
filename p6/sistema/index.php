ebf08ea0e90797e9fda3ec34011df406bbe19b22
IvanGomezSuarez <59507021+IvanGomezSuarez@users.noreply.github.com>
2020-11-09 17:40:28 +0100

    iconos e inicio del dashboard

--- p6/sistema/index.php
+++ p6/sistema/index.php
@@ -4,12 +4,13 @@
 <head>
 	<meta charset="UTF-8">
 	<?php include "includes/scripts.php"; ?>
-	
+	<link rel="stylesheet" type = "text/css" href="css/styleDashboard.css">
 
 	<title>Administracion</title>
 </head>
 <body>
 	<?php include "includes/header.php"; ?>	
+	
 	<section id="container">
 		<div>
 			<h1 class="titlePanelControl">Panel de control</h1>
@@ -55,16 +56,48 @@
 				</p>
 			</a>
 		
-		
-		
-		
-		
 		</div>
-	</section>
-
-
-
-	
+	</section>	
 	<?php include "includes/footer.php"; ?>
 </body>
 </html>
+
+<style>
+.titlePanelControl{
+	width: 100%;
+	background: #fff;
+	padding: 5px 15px;
+    margin-bottom: 10px;
+    font-size: 18px !important;
+    color: #0A4661;
+}
+
+.dashboard{
+ 
+    display: flex;
+    justify-content: space-around ;
+    width: 100%;
+    margin: auto;
+}
+
+.dashboard a{
+    color: #898989;
+    width: calc(100% / 2);
+    padding: 20px;
+    background-color: #fff;
+    font-size: 25pt;
+	text-align: center;
+	width: 100%;
+
+}
+
+.dashboard p{
+    color: #3279a7;
+}
+
+.dashboard a span{
+    font-weight: bold;
+    font-size: 20pt;
+}
+
+</style>
