0c91124d94194a88276ed756a210dc702013392c
IvanGomezSuarez <59507021+IvanGomezSuarez@users.noreply.github.com>
2020-11-10 21:29:01 +0100

    iconos e inicio del dashboard

--- p6/sistema/index.php
+++ p6/sistema/index.php
@@ -12,12 +12,13 @@
 	<?php include "includes/header.php"; 
 	include "../conexion.php";
 	$query_dash = mysqli_query($conection,"CALL dataDashboard();");
-	echo $sql;exit;
+	//echo $sql;exit;
 	$result_dash = mysqli_num_rows($query_dash);
 	if(result_dash > 0){
 		$data_dash = mysqli_fetch_assoc($query_dash);
 		mysqli_close($conection);
 	}
+	print_r($data_dash);
 	?>	
 	
 	<section id="container">
@@ -27,10 +28,10 @@
 		</div>
 		<div class="dashboard">
 			<a href="listado_usuarios.php">
-				<i class="fas fa-users"></i>
+				<i class="fas fa-users "></i>
 				<p>
 					<strong>Administradores</strong><br>
-					<span><?echo $data_dash['admins']; ?></span>
+					<span><? echo $data_dash['admins']; ?></span>
 				</p>
 			</a>
 			<div class="dashboard">
@@ -38,7 +39,7 @@
 				<i class="fas fa-users"></i>
 				<p>
 					<strong>Profesores</strong><br>
-					<span><?echo $data_dash['profesores']; ?></span>
+					<span><? echo $data_dash['profesores']; ?></span>
 				</p>
 			</a>
 			<div class="dashboard">
@@ -46,7 +47,7 @@
 				<i class="fas fa-users"></i>
 				<p>
 					<strong>Cursos</strong><br>
-					<span><?echo $data_dash['cursos']; ?></span>
+					<span><? echo $data_dash['cursos']; ?></span>
 				</p>
 			</a>
 			<div class="dashboard">
@@ -54,7 +55,7 @@
 				<i class="fas fa-users"></i>
 				<p>
 					<strong>Asignaturas</strong><br>
-					<span><?echo $data_dash['asignaturas']; ?></span>
+					<span><? echo $data_dash['asignaturas']; ?></span>
 				</p>
 			</a>
 			<div class="dashboard">
@@ -77,8 +78,37 @@
 			</div>
 				<div class="ContainerPerfil">
 					<div class="ContainerDataUser">
-					<i class="fas fa-users"></i>
+					<i class="fas fa-users fa-7x "></i>
 					</div>
+						<div class="divDataUser">
+							<h4>Información personal"</h4>
+						<div>
+						<label>Nombre:</label> <span>Ivan</span>
+						</div>
+						<div>
+						<label>Nombre:</label> <span>Ivan</span>
+						</div>
+						<h4>Datos de usuario</h4> <span>Ivan2020</span>
+						<h4>Cambiar contraseña</h4> 
+						<form-action="" method="post" name="frmChangePass" id="frmChangePass">
+							<div>
+							<input type="password" name="txtPassUser" id="txtPassUser" 
+							placeholder="Contreseña actual" required>
+							</div>
+							<div>
+							<input type="password" name="txtNewPassUser" id="txtNewPassUser" 
+							placeholder="Nueva contraseña" required>
+							</div>
+							<div>
+							<input type="password" name="txtPassConfirm" id="txtPassConfirm" 
+							placeholder="Confirmar contraseña" required>
+							</div>
+							<div>
+							<button type="password" class="btn_save btnChangePass"><i class="fas fa-key"></i>Cambiar contraseña</button>
+							</div>
+						</form>
+
+				</div>
 				</div>
 			</div>
 		</div>	
@@ -130,7 +160,21 @@
     font-size: 20pt;
 }
 
-
-
+.containerPerfil{
+	display: flex;
+	display: -webkit-flex;
+	justify-content: space-around;
+	align-items: flex-start;
+	flex-wrap: wrap;
+}
+.ContainerDataUser{
+	width: 500px;
+	background-color: #fff;
+	padding: 20px;
+}
+.divDataUser{
+	padding: 10px;
+	margin: auto;
+}
 
 </style>
