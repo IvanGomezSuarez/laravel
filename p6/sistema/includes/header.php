07a5159e2aec13930e0bf843fd2ebbc7570b1cfb
IvanGomezSuarez <59507021+IvanGomezSuarez@users.noreply.github.com>
2020-11-16 23:42:53 +0100

    pestaña de matricula. con form-modal

--- p6/sistema/includes/header.php
+++ p6/sistema/includes/header.php
@@ -20,19 +20,21 @@ if(!isset($_SESSION['role'])){
 				<a href="salir.php"><img class="close" src="img/salir.png" alt="Salir del sistema" title="Salir"></a>
 			</div>
 		</div>
-        <?php include "nav.php"; ?>
+		<?php include "nav.php"; ?>
+		<?php include "js/functions.js"; ?>
 	</header>
 
 	<div class="modal">
 		<div class="bodymodal">
-			<form action="" method="post" name="form_add_enrollment" id="form_add_enrollment">
-				<h1><i class="fas fa cubes" style="font-size:45pt;"></i><br> Guardar matrícula</h1>
+			<form action="" method="post" name="form_add_enrollment" id="form_add_enrollment" onsubmit="
+			event.preventDefault(); sendDataMatricula();">
+				<h1><i class="fas fa-save" style="font-size:45pt;"></i><br> Guardar matrícula</h1></br>
 				<h2 class="nombre_alumno">alejandro</h2></br>
 				<h2 class="nombre_curso">Matematicas</h2></br>
 				<input type="hidden" name="action" value="addMatricula" required>
-				<div class="alert alertAddMatricula"><p>Alerta de accion</p></div>
-				<button type="submit" class="btn_new"><i class="fas fa-plus"></i> Guardar matrícula</button>
-				<a ref="#" class="btn_ok closeModal"><i class="fas fa-ban"></i> Cerrar</a>
+				<div class="alert alertAddMatricula"></div>
+				<button type="submit" class="btn_new"><i class="fas fa-save"></i> Guardar</button>
+				<a ref="#" class="btn_ok closeModal" onclick="closeModal();"><i class="fas fa-ban"></i> Cerrar</a>
 				
 			</form>
 		</div>
--- p6/sistema/js/functions.js
+++ p6/sistema/js/functions.js
@@ -2,9 +2,17 @@ $(document.readyState(function(){
 
 //modal-form add matricula
 
-$('.addMatricula').click(function(event){
-    event.preventDefault();
-    echo("hola mund");
+$('.addMatricula').click(function(e){
+    //e.preventDefault();
+    $('.modal').fadeIn();
 });
 
+
 }));
+
+function closeModal(){
+    $('.modal').fadeOut();
+
+}
+
+
