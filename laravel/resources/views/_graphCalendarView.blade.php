0a04e14ffee354e82d09ca7c782b6d0da2b6a2e1
IvanGomezSuarez <59507021+IvanGomezSuarez@users.noreply.github.com>
2020-11-28 18:22:36 +0100

    creacion de views

---
+++ laravel/resources/views/_graphCalendarView.blade.php
@@ -0,0 +1,135 @@
+<?php 
+
+/* si no hemos dado a next o prev cargamos año y mes por defecto que son los en curso*/
+  if(!isset($_GET['mes'])){
+            $_SESSION['month']=date("m");   /* el mes de inicio que es el actual */
+            $_SESSION['year'] = date("Y"); /*  el año de inicio que es el actual */           
+        }
+
+include MOD.'_graphCalendarModel.php';
+include CONT.'_graphCalendarController.php';
+
+
+?>
+<style>
+ 
+    td{
+       width: 14.28vw;
+       height: 13.28vw;
+       border: 1px solid #9a74741f;
+       position: relative;
+    }
+    td.blur, td.blaur{
+        visibility: hidden;
+    }
+    span.dayname, a.date {
+        position: absolute;
+        top: 0;
+        font-size: 15px;
+       
+    }
+    .asign{
+     color:white;
+    font-size: 1.1vw;
+    }
+    tr.days td{
+        height: 30px;
+        margin-left: 10%;
+       
+       
+    }
+    
+</style>
+<br><br>
+<div class="container">
+    
+<?php 
+    if ($size>1){  
+
+?>    
+    
+    
+<section>	
+    <h3><?php 
+    
+    
+    echo date("d"). '/'.$month.'/'.$year;
+    
+    ?></h3>
+    <a class="btn btn-primary btn-sm" href="?pag=_graphCalendarView.php&mes=prev">Previo</a> <a class="btn btn-primary btn-sm" href="?pag=_graphCalendarView.php&mes=next">Siguiente</a>
+
+    <table class="month mt-4">
+	<tr class="days">
+            <td><span class='dayname'>Lun</span></td>
+            <td><span class='dayname'>Mar</td>
+            <td><span class='dayname'>Mie</td>
+            <td><span class='dayname'>Jue</td>
+            <td><span class='dayname'>Vie</td>
+            <td><span class='dayname'>Sab</td>
+            <td><span class='dayname'>Dom</td>
+	</tr>
+        <?php 
+	$today = date(" d "); // Current day
+	//$month = date("$m"); // Current month
+	//$year = date("Y"); // Current year
+        
+	$days = cal_days_in_month(CAL_GREGORIAN,$month,$year); // Days in current month
+	$lastmonth = date("t", mktime(0,0,0,$month-1,1,$year)); // Days in previous month
+	
+	$start = date("N", mktime(0,0,0,$month,1,$year)); // Starting day of current month
+	$finish = date("N", mktime(0,0,0,$month,$days,$year)); // Finishing day of  current month
+	$laststart = $start - 1; // Days of previous month in calander
+	
+	$counter = 1;
+	$nextMonthCounter = 1;
+
+        
+	if($start > 5){	$rows = 6; }else {$rows = 5; }
+
+	for($i = 1; $i <= $rows; $i++){
+		echo '<tr class="week">';
+		for($x = 1; $x <= 7; $x++){				
+			
+			if(($counter - $start) < 0){
+				$date = (($lastmonth - $laststart) + $counter);
+				$class = 'class="blur"';
+			}else if(($counter - $start) >= $days){
+				$date = ($nextMonthCounter);
+				$nextMonthCounter++;
+				
+				$class = 'class="blaur"';
+					
+			}else {
+				$date = ($counter - $start + 1);
+				if($today == $counter - $start + 1){
+					$class = 'class="today"';
+				}
+			}
+				
+			
+			echo '<td '.$class.'><a class="date">'. $date . '</a>';
+                        
+                        for ($a=1;$a<sizeof($horario);$a++){
+                            $display='block;';
+                            if (($horario[$a]->day) == ($year.'-'.$month.'-'.$date)){
+                                for ($w=1;$w<$totalAsignaturasCurso;$w++){
+                                     if (($arrayAsignaturasCurso[$w]->id_class)==($horario[$a]->id_class)){
+                                     print_r('<div class="asign" style="display:'.$display.'background-color:'.cargamosAsignaturas($horario[$a]->id_class)[1]->color.'">'. cargamosAsignaturas($horario[$a]->id_class)[1]->name.'<br>'.$horario[$a]->time_start.'-'.$horario[$a]->time_end).'</div>';
+                                    }
+                                } 
+                            }
+                        } 
+                        
+                        echo '</td>';
+		
+			$counter++;
+			$class = '';
+		}
+		echo '</tr>';
+	}
+	
+?>
+</table>
+</section>	
+    <?php }?>
+</div>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,25 @@
 <?php
 
-
+use App\Http\Controllers\_adminAlumnController;
+use App\Http\Controllers\_adminMatriculasController;
+use App\Http\Controllers\_asignaturasController;
+use App\Http\Controllers\_asigxcursoController;
+use App\Http\Controllers\_calendarioController;
+use App\Http\Controllers\_cursosController;
+use App\Http\Controllers\_graphCalendarController;
+use App\Http\Controllers\_indexController;
+use App\Http\Controllers\_loginController;
+use App\Http\Controllers\_profeAsignaController;
+use App\Http\Controllers\_profesoresController;
+use App\Http\Controllers\_registerController;
+use App\Http\Controllers\_studAsignaturasController;
+use App\Http\Controllers\_studCalendarioController;
+use App\Http\Controllers\_studConfigController;
+use App\Http\Controllers\_studCursosController;
+use App\Http\Controllers\_studentAdminController;
+use App\Http\Controllers\_studWelcomeController;
+use App\Http\Controllers\_userConfigController;
+use App\Http\Controllers\_welcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +32,29 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', _welcomeController::class);
+Route::get('/', _adminAlumnController::class);
+Route::get('/', _adminMatriculasController::class);
+Route::get('/', _asigxcursoController::class);
+Route::get('/', _asignaturasController::class);
+Route::get('/', _calendarioController::class);
+Route::get('/', _cursosController::class);
+Route::get('/', _graphCalendarController::class);
+Route::get('/', _indexController::class);
+Route::get('/', _loginController::class);
+Route::get('/', _profesoresController::class);
+Route::get('/', _profeAsignaController::class);
+Route::get('/', _registerController::class);
+Route::get('/', _studAsignaturasController::class);
+Route::get('/', _studCalendarioController::class);
+Route::get('/', _studConfigController::class);
+Route::get('/', _studCursosController::class);
+Route::get('/', _studWelcomeController::class);
+Route::get('/', _studentAdminController::class);
+Route::get('/', _userConfigController::class);
+
+
 
 
 
