123769ce068f6950a50a50cbc3782bba6601ddcb
jlopezcoll <jlopezcoll@uoc.edu>
2020-12-22 04:01:17 +0100

    new class LoginResponse

---
+++ app/Http/Responses/LoginResponse.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Illuminate\Support\Facades\Auth;
+use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
+
+class LoginResponse implements LoginResponseContract
+{
+
+    public function toResponse($request)
+    {
+        
+        // below is the existing response
+        // replace this with your own code
+        // the user can be located with Auth facade
+        $role = Auth::user()->role;
+        $checkrole = explode(',', $role);  // However you get role
+       
+        if (in_array('admin', $checkrole)) {
+
+            //Session::put('isadmin', 'admin');
+            
+           return redirect('profesores');
+        } else {
+            return redirect('welcome');
+        }
+        
+        return $request->wantsJson()
+                    ? response()->json(['two_factor' => false])
+                    : redirect()->intended(config('fortify.home'));
+    }
+
+}
