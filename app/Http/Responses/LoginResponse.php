d6f51a487d24d47363c1b3fdc1a4a2d496301aef
IvanGomezSuarez <59507021+IvanGomezSuarez@users.noreply.github.com>
2020-12-22 17:22:24 +0100

    añadir responseLogin

---
+++ app/Http/Responses/LoginResponse.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
+
+class LoginResponse implements LoginResponseContract
+{
+
+    public function toResponse($request)
+    {
+        
+        // below is the existing response
+        // replace this with your own code
+        // the user can be located with Auth facade
+        $role = Auth::user()->role;
+        $checkrole = explode(',', $role);  // However you get role
+       
+        if (in_array('admin', $checkrole)) {
+
+            Session::put('role', 'admin');
+
+           return redirect('profesores');
+        } else {
+            return redirect('dashboard');
+        }
+        
+        return $request->wantsJson()
+                    ? response()->json(['two_factor' => false])
+                    : redirect()->intended(config('fortify.home'));
+    }
+
+}
